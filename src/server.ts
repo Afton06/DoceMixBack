ad5f7241457e1354b3b5f7fe5b84912aa5c6187b
Francesco Gris <francescofgris@gmail.com>
2026-03-27 21:32:19 -0300

    CRUD completo de Administrador

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import categoriaRouter from './routes/categoria.routes'
 import produtoRouter from './routes/produto.routes'
 import usuarioRouter from './routes/usuario.routes'
 import authRouter from './routes/auth.routes'
+import administradorRouter from './routes/administrador.routes'
 
 const app = express()
 const PORT = 3000
@@ -13,6 +14,7 @@ app.use('/categorias', categoriaRouter)
 app.use('/produtos', produtoRouter)
 app.use('/usuarios', usuarioRouter)
 app.use('/auth', authRouter)
+app.use('/administradores', administradorRouter)
 
 app.listen(PORT, () => {
   console.log(`Servidor da Estação do Forno rodando na porta ${PORT}`)
