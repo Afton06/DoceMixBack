0c7d5901768284dbf061fac4e2dd42fee47c1056
Francesco Gris <francescofgris@gmail.com>
2026-03-27 21:14:50 -0300

    autenticação com login e JWT

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import categoriaRouter from './routes/categoria.routes'
 import produtoRouter from './routes/produto.routes'
 import usuarioRouter from './routes/usuario.routes'
+import authRouter from './routes/auth.routes'
 
 const app = express()
 const PORT = 3000
@@ -11,7 +12,8 @@ app.use(express.json())
 app.use('/categorias', categoriaRouter)
 app.use('/produtos', produtoRouter)
 app.use('/usuarios', usuarioRouter)
+app.use('/auth', authRouter)
 
 app.listen(PORT, () => {
-  console.log(`Servoidor da Estação do Forno rodando na porta ${PORT}`)
+  console.log(`Servidor da Estação do Forno rodando na porta ${PORT}`)
 })
